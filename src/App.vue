8de511ed09cd513af7f747b4e214837d2e168a93
Michael Efemena <mefemena@gmail.com>
2020-12-17 16:07:43 +0100

    add Todos and TodoItem components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-
-  </div>
+  <div id="app">{{msg}}</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-    
+  name: "App",
+  components: {},
+  data() {
+    return {
+      msg: "Hello"
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +22,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} 
 </style>
