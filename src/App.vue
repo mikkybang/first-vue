03c8c4d22898debd71dffd317f39f29c9c562a2f
Michael Efemena <mefemena@gmail.com>
2020-12-17 16:51:04 +0100

    made add todo component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
-  <div id="app">{{msg}}</div>
+  <div id="app">
+    <Header />
+    <AddTodo />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
+import Header from "./components/layouts/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
   data() {
     return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ],
       msg: "Hello"
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 };
 </script>
@@ -22,5 +56,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} 
+}
 </style>
